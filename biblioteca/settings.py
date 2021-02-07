33f177158a0ffe830b49ccd044d40a1474d0305b
andreengenharia <andreengenharia.aa@gmail.com>
2021-02-07 17:09:13 -0300

    UPDATE ALLOWED_HOST

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['infinite-mesa-66713.herokuapp.com']
+ALLOWED_HOSTS = ['<your app URL without the https:// prefix>.herokuapp.com','127.0.0.1']
+
+#ALLOWED_HOSTS = ['infinite-mesa-66713.herokuapp.com']
 
 
 # Application definition
